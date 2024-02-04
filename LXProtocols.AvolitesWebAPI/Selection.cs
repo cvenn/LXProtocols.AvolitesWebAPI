9f8fd428be477b09a7e4bb3f045c48ff6a5b1625
cvenn <cv.gh@vennery.com>
2024-02-04 01:08:25 +0000

    Added SelectFixtureFromHandle

--- LXProtocols.AvolitesWebAPI/Selection.cs
+++ LXProtocols.AvolitesWebAPI/Selection.cs
@@ -28,6 +28,14 @@ namespace LXProtocols.AvolitesWebAPI
         /// Allows you to select a range fixture in the editor, when you select a fixture you can modify values for the selected fixtures.
         /// </summary>
         /// <param name="handle">The handle.</param>
+        public async Task SelectFixtureFromHandle(HandleReference handle) {
+            await http.GetAsync($"titan/script/2/Selection/Context/Programmer/SelectFixture?{handle.ToQueryArgument("handle")}");
+        }
+
+        /// <summary>
+        /// Allows you to select a range fixture in the editor, when you select a fixture you can modify values for the selected fixtures.
+        /// </summary>
+        /// <param name="handle">The handle.</param>
         public async Task SelectFixturesFromHandles(IEnumerable<HandleReference> handle) {
             await http.GetAsync($"titan/script/2/Selection/Context/Programmer/SelectFixturesFromHandles?{handle.ToQueryArgument("handleList")}");
         }
